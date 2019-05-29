f2d3e0ca11d7b39a55f5358ea3bd29c713de0660
zykzyh <18302010056@fudan.edu.cn>
2019-05-29 23:54:18 +0800

    add lab9

--- lab8/functions.inc.php
+++ lab8/functions.inc.php
@@ -1,8 +1,14 @@
 <?php
-
-    function outputOrderRow($file, $title, $quantity, $price) {
+function outputOrderRow($file, $title, $quantity, $price) {
         echo "<tr>";
+        $amount = $quantity * $price;
         //TODO
+        echo "<td><img src= 'images/books/tinysquare/$file''></td>";
+        echo "<td class=\"mdl-data-table__cell--non-numeric\">$title</td>";
+        echo "<td>$quantity</td>";
+        echo "<td>$price</td>";
+        echo "<td>$amount</td>";
         echo "</tr>";
+        return $amount;
     }
 ?>
--- lab8/lab8.php
+++ lab8/lab8.php
@@ -12,6 +12,12 @@ $shippingFlatAmount = 200;
 $subtotal = 0;
 $shipping = 0;
 $grandTotal = 0;
+$subtotal = $price1 * $quantity1 + $price2 * $quantity2 +$price3 * $quantity3 + $price4 * $quantity4;
+if ($subtotal >= $shippingThreshold )
+    $shipping = 100;
+else
+    $shipping = $shippingFlatAmount;
+$grandTotal = $subtotal + $shipping;
 
 ?>
 
@@ -37,9 +43,10 @@ $grandTotal = 0;
 <!-- You should decide where to add the `header.inc.php` and `left.inc.php` -->
 <div class="mdl-layout mdl-js-layout mdl-layout--fixed-drawer
             mdl-layout--fixed-header">
-
+    <?php include 'left.inc.php'; ?>
   <main class="mdl-layout__content mdl-color--grey-50">
     <header class="mdl-color--blue-grey-200">
+    <?php include 'header.inc.php'; ?>
       <h4>Order Summaries</h4>
       <p>Examine your customer orders</p>
     </header>   
